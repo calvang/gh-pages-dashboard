b8eb9c031e3148bf27c282a2c4b9e1defe19b7e1
calvang <calvin.huang5@gmail.com>
2020-08-20 00:43:28 -0400

    Updates

---
+++ precache-manifest.a14b2bf968527c9ea7b1ccd1c4dafe96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1abfe32afb473b005edcc7f4140f250",
+    "url": "/gh-pages-dashboard/index.html"
+  },
+  {
+    "revision": "2ea8f1e91e0cfa79bbdb",
+    "url": "/gh-pages-dashboard/static/css/main.c37fff89.chunk.css"
+  },
+  {
+    "revision": "a9d97450de54c8158705",
+    "url": "/gh-pages-dashboard/static/js/2.971f7480.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/gh-pages-dashboard/static/js/2.971f7480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea8f1e91e0cfa79bbdb",
+    "url": "/gh-pages-dashboard/static/js/main.06fba0b7.chunk.js"
+  },
+  {
+    "revision": "869a36146f43cb9e8f58",
+    "url": "/gh-pages-dashboard/static/js/runtime-main.d7323579.js"
+  }
+]);
